a08aed213ca6adcb61edd3b89aa27e91a2d06904
Young Jea Cho <btimpeul@gmail.com>
2021-03-29 14:01:49 +0900

    add 8958.py

--- baekjoon/python/8958.py
+++ baekjoon/python/8958.py
@@ -1,12 +1,13 @@
 n = int(input())
 for _ in range(n):
-    ox = input()
-    score = 0
-    cnt = 1
-    for i in range(0, len(ox)):
-        if ox[i] == "O":
-            score += cnt
+    scores = input().split(" ")
+    allScore = 0
+    cnt = 0
+    for i in range(1 ,len(scores)):
+        allScore += int(scores[i])
+    mean = allScore / int(scores[0])
+    for i in range(1, len(scores)):
+        if int(scores[i]) > mean:
             cnt += 1
-        elif ox[i] == "X":
-            cnt = 1
-    print(f"{score}")
+    meanstudent = cnt / int(scores[0]) * 100
+    print("{:.3f}%".format(meanstudent))
